5339bba43a07e824a781e3ee9a08bf109bd6f12b
mmcc91 <121430505+mmcc91@users.noreply.github.com>
2024-02-23 10:52:58 +0100

    debut du projet

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import ErrorPage from "./error-page";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root from "./routes/root";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
