99cf6e0215c288f1816f47641d05865466583b12
mmcc91 <121430505+mmcc91@users.noreply.github.com>
2024-03-03 17:03:12 +0100

    debut de page error

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import ErrorPage from "./error-page";
+import ErrorPage from "./pages/error-page";
 import {
   createBrowserRouter,
   RouterProvider,
--- src/pages/error-page.jsx
+++ src/pages/error-page.jsx
@@ -1,16 +1,26 @@
 import { useRouteError } from "react-router-dom";
-
+import "../pages/error.scss";
+import Header from "../components/header/Header.jsx";
+import Footer from "../components/footer/Footer.jsx";
+import { NavLink } from "react-router-dom";
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
-
+ 
   return (
+     
     <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+        <Header />
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+     
+        <NavLink to ="/Accueil"  >
+            <p>Retourner sur la page d’accueil</p>
+          </NavLink>
+    <Footer />
     </div>
   );
 }
+
+
+   
