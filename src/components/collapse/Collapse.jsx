63a1780556fb21d7472fd5854c05c0dcd394d5d1
mmcc91 <121430505+mmcc91@users.noreply.github.com>
2024-03-11 16:44:28 +0100

    carousel fixed

--- src/components/collapse/Collapse.jsx
+++ src/components/collapse/Collapse.jsx
@@ -4,24 +4,27 @@ import flecheUp from "../../assets/images/arrow_up.png";
 import React, { useState } from "react";
 
 const Collapse = ({ title, info }) => {
-const [toggle, setToggle] = useState(false);
-
-
+  const [toggle, setToggle] = useState(false);
 
   return (
     <div className="collapse">
       <h3 className="collapse_titre" onClick={() => setToggle(!toggle)}>
         {title}
-    
-        <img src={toggle ? flecheUp : flecheBas} alt="fleche " className="fleche" />
+        <img src={toggle ? flecheUp : flecheBas} alt="fleche " className={`fleche ${toggle ? "fleche_nonbascule" : ""}`} />
       </h3>
       <div className={toggle ? "collapse_bascule" : "collapse_nonbascule"}>
-        {Array.isArray(info) ? 
-        info.map((text, index) =>  (<> {text} <br /></>) ) : info}</div>
+        {Array.isArray(info) ? (
+          info.map((text, index) => (
+            <span key={index} className="text">
+              {text} <br />
+            </span>
+          ))
+        ) : (
+          <span className="text">{info}</span>
+        )}
+      </div>
     </div>
   );
 };
-
-
 
 export default Collapse;
